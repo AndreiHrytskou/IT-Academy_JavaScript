97e331133931bfbcea0e9212ebfaec1bd9154bc0
Andrei Hrytskou <andreihrytskou@gmail.com>
2021-11-18 21:27:39 +0300

    done task07

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const arr = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8];
-const newArr = arr.flat(Infinity);
-const reducer = (a, b) => {
-  return a + b;
-};
-console.log(newArr.reduce(reducer));
+function vowels() {
+  let a = 0;
+  const rusVowels = ["у", "ё", "е", "ы", "а", "о", "э", "я", "и", "ю"];
+  const word = prompt("Введите слово");
+  for (let i = 0; i < word.length; i++)
+    for (let j = 0; j < rusVowels.length; j++)
+      if (word[i] === rusVowels[j]) {
+        ++a;
+        break;
+      }
+  return a ? a : "нет русских гласных букв";
+}
+alert(vowels());
